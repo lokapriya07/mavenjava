733dd9a74835ee54018084a61f8d047dbde0f77c
Loka <lokapriya7852@gmail.com>
2024-11-14 21:57:42 +0530

    files added

---
+++ src/main/java/com/app/maven/mavenproj/App.java
@@ -0,0 +1,13 @@
+package com.app.maven.mavenproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Lokapriya 22bd1a0518" );
+    }
+}
